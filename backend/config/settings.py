3e32f8091dc7b84ccc934c382aebf01dc1682124
adam-echols <104486156+adamechol@users.noreply.github.com>
2022-07-05 11:24:48 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = 'django-insecure-ov#r^^&xv&^0vmc(zj&h_t^$*52@8jicn=%*z*@s-=li!s_p@=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'forum-prod-api.herokuapp.com']
+ALLOWED_HOSTS = ['https://backend-adam-cooltees.herokuapp.com/']
 CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'http://127.0.0.1:3000', 'https://forum-prod-frontend.herokuapp.com']
+CORS_ALLOWED_ORIGINS = ['https://frontend-adam-cooltees.herokuapp.com/']
 
 # Application definition
 
